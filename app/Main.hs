3af07bae6635a1542bf98c4efd13581d4b272273
Dustin Sallings <dustin@spy.net>
2020-03-28 18:14:47 -0700

    Split out fixup

--- app/Main.hs
+++ app/Main.hs
@@ -11,36 +11,27 @@ module Main where
 
 import           Control.Monad          (when)
 import           Control.Monad.Catch    (bracket_)
-import           Control.Monad.Fail     (MonadFail (..))
 import           Control.Monad.IO.Class (MonadIO (..))
 import           Control.Monad.Logger   (LogLevel (..), MonadLoggerIO (..),
                                          filterLogger, runStderrLoggingT)
-import           Control.Monad.Reader   (ReaderT (..), ask, asks, runReaderT)
-import qualified Data.Aeson             as J
-import qualified Data.ByteString.Lazy   as BL
+import           Control.Monad.Reader   (ReaderT (..), asks, runReaderT)
 import           Data.Cache             (newCache)
-import qualified Data.HashMap.Strict    as HM
 import           Data.List              (intercalate)
 import           Data.Maybe             (fromMaybe)
-import           Data.Scientific        (fromFloatDigits)
-import           Data.String            (fromString)
 import qualified Data.Text              as T
-import qualified Data.Text.Encoding     as TE
-import           Database.SQLite.Simple (SQLData (..), Statement, columnCount,
-                                         columnName, nextRow, withConnection,
-                                         withStatement)
+import           Database.SQLite.Simple (withConnection)
 import           Options.Applicative    (Parser, argument, auto, execParser,
                                          fullDesc, help, helper, info, long,
                                          metavar, option, progDesc, short,
                                          showDefault, some, str, strOption,
                                          switch, value, (<**>))
-import           Prelude                hiding (fail)
 import           System.Clock           (TimeSpec (..))
 import           System.IO              (hFlush, hGetEcho, hSetEcho, stdin,
                                          stdout)
 
 import           GoPro.AuthDB
 import           GoPro.Commands
+import           GoPro.Commands.Fixup
 import           GoPro.Commands.Sync
 import           GoPro.Commands.Upload
 import           GoPro.Commands.Web
@@ -88,44 +79,6 @@ runReauth = do
   db <- asks dbConn
   res <- refreshAuth =<< loadAuth db
   updateAuth db res
-
-runFixup :: GoPro ()
-runFixup = do
-  db <- asks dbConn
-  env <- ask
-  args <- asks (optArgv . gpOptions)
-  when (length args /= 1) $ fail "I need a query to run"
-  let [query] = fromString <$> args
-  logDbg $ "Query: " <> tshow query
-  liftIO $ withStatement db query (\st -> runIO env (needful st))
-
-    where
-      needful :: Statement -> EnvM ()
-      needful st = do
-        cnum <- liftIO $ columnCount st
-        cols <- mapM (liftIO . columnName st) [0 .. pred cnum]
-        process cols
-          where
-            process :: [T.Text] -> EnvM ()
-            process cols = do
-              r <- liftIO (nextRow st :: IO (Maybe [SQLData]))
-              logDbg $ tshow r
-              maybe (pure ()) (\rs -> store (zip cols rs) >> process cols) r
-            store :: [(T.Text, SQLData)] -> EnvM ()
-            store stuff = do
-              mid <- case lookup "media_id" stuff of
-                       (Just (SQLText m)) -> pure m
-                       _ -> fail ("no media_id found in result set")
-              logInfo $ "Fixing " <> tshow mid
-              (J.Object rawm) <- medium mid
-              let v = foldr up rawm (filter (\(k,_) -> k /= "media_id") stuff)
-              logDbg $ TE.decodeUtf8 . BL.toStrict . J.encode $ v
-              putMedium mid (J.Object v)
-            up (name, (SQLInteger i)) = HM.insert name (J.Number (fromIntegral i))
-            up (name, (SQLFloat i))   = HM.insert name  (J.Number (fromFloatDigits i))
-            up (name, (SQLText i))    = HM.insert name (J.String i)
-            up (name, SQLNull)        = HM.insert name J.Null
-            up (_,    (SQLBlob _))    = error "can't do blobs"
 
 run :: String -> GoPro ()
 run c = fromMaybe (liftIO unknown) $ lookup c cmds
---
+++ src/GoPro/Commands/Fixup.hs
@@ -0,0 +1,66 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralisedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TupleSections              #-}
+{-# LANGUAGE UndecidableInstances       #-}
+
+module GoPro.Commands.Fixup where
+
+import           Control.Monad          (when)
+import           Control.Monad.Fail     (MonadFail (..))
+import           Control.Monad.IO.Class (MonadIO (..))
+import           Control.Monad.Reader   (ask, asks)
+import qualified Data.Aeson             as J
+import qualified Data.ByteString.Lazy   as BL
+import qualified Data.HashMap.Strict    as HM
+import           Data.Scientific        (fromFloatDigits)
+import           Data.String            (fromString)
+import qualified Data.Text              as T
+import qualified Data.Text.Encoding     as TE
+import           Database.SQLite.Simple (SQLData (..), Statement, columnCount,
+                                         columnName, nextRow, withStatement)
+import           Prelude                hiding (fail)
+
+import           GoPro.Commands
+import           GoPro.Plus.Media
+
+runFixup :: GoPro ()
+runFixup = do
+  db <- asks dbConn
+  env <- ask
+  args <- asks (optArgv . gpOptions)
+  when (length args /= 1) $ fail "I need a query to run"
+  let [query] = fromString <$> args
+  logDbg $ "Query: " <> tshow query
+  liftIO $ withStatement db query (\st -> runIO env (needful st))
+
+    where
+      needful :: Statement -> EnvM ()
+      needful st = do
+        cnum <- liftIO $ columnCount st
+        cols <- mapM (liftIO . columnName st) [0 .. pred cnum]
+        process cols
+          where
+            process :: [T.Text] -> EnvM ()
+            process cols = do
+              r <- liftIO (nextRow st :: IO (Maybe [SQLData]))
+              logDbg $ tshow r
+              maybe (pure ()) (\rs -> store (zip cols rs) >> process cols) r
+            store :: [(T.Text, SQLData)] -> EnvM ()
+            store stuff = do
+              mid <- case lookup "media_id" stuff of
+                       (Just (SQLText m)) -> pure m
+                       _ -> fail ("no media_id found in result set")
+              logInfo $ "Fixing " <> tshow mid
+              (J.Object rawm) <- medium mid
+              let v = foldr up rawm (filter (\(k,_) -> k /= "media_id") stuff)
+              logDbg $ TE.decodeUtf8 . BL.toStrict . J.encode $ v
+              putMedium mid (J.Object v)
+            up (name, (SQLInteger i)) = HM.insert name (J.Number (fromIntegral i))
+            up (name, (SQLFloat i))   = HM.insert name  (J.Number (fromFloatDigits i))
+            up (name, (SQLText i))    = HM.insert name (J.String i)
+            up (name, SQLNull)        = HM.insert name J.Null
+            up (_,    (SQLBlob _))    = error "can't do blobs"
