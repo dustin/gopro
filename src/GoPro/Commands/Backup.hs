407a3eedc182891f429cfbe51ca746e6daaf8a2b
Dustin Sallings <dustin@spy.net>
2020-04-05 23:07:56 -0700

    Use SQS to run backups.

--- src/GoPro/Commands/Backup.hs
+++ src/GoPro/Commands/Backup.hs
@@ -1,25 +1,23 @@
 module GoPro.Commands.Backup (runBackup) where
 
 
-import           Conduit                 (sinkFile)
 import           Control.Lens
 import           Control.Monad           (void, when)
-import           Control.Monad.IO.Class  (MonadIO (..))
 import           Control.Monad.Reader    (asks)
 import           Control.Monad.Trans.AWS (Credentials (..), Region (..),
-                                          ToBody (..), envRegion, hashedFile,
-                                          newEnv, paginate, runAWST,
+                                          envRegion, newEnv, paginate, runAWST,
                                           runResourceT, send)
-import           Data.Conduit            (runConduit, runConduitRes, (.|))
+import qualified Data.Aeson              as J
+import qualified Data.ByteString.Lazy    as BL
+import           Data.Conduit            (runConduit, (.|))
 import qualified Data.Conduit.List       as CL
 import qualified Data.Set                as Set
 import           Data.String             (fromString)
 import           Data.Text               (Text, isSuffixOf, pack, unpack)
+import qualified Data.Text.Encoding      as TE
 import           Network.AWS.S3
-import           Network.HTTP.Simple     (getResponseBody, httpSource,
-                                          parseRequest)
-import           System.Directory        (createDirectoryIfMissing, removeFile)
-import           System.FilePath.Posix   (takeBaseName, takeDirectory, (</>))
+import           Network.AWS.SQS         (sendMessage)
+import           System.FilePath.Posix   (takeBaseName, takeDirectory)
 
 import           GoPro.Commands
 import           GoPro.DB
@@ -29,7 +27,7 @@ type Derivative = (MediumID, Text)
 
 allDerivatives :: BucketName -> GoPro [Derivative]
 allDerivatives bucketName = do
-  awsenv <- newEnv Discover <&> set envRegion NorthVirginia
+  awsenv <- newEnv Discover <&> set envRegion Oregon
   runResourceT . runAWST awsenv $
     runConduit $ paginate (listObjectsV2 bucketName & lovPrefix ?~ "derivatives/")
     .| CL.concatMap (view lovrsContents)
@@ -41,32 +39,35 @@ allDerivatives bucketName = do
   where toDir t = let s = unpack t in
                     (pack . takeBaseName . takeDirectory $ s, pack $ takeBaseName s)
 
-storeDerivative :: BucketName -> MediumID -> String -> GoPro ()
-storeDerivative bucketName mid d = do
-  liftIO $ createDirectoryIfMissing True ".cache"
+type QueueURL = Text
+
+storeDerivative :: QueueURL -> BucketName ->  MediumID -> String -> GoPro ()
+storeDerivative qrl (BucketName bucketName) mid d = do
   Just var <- preview (fileStuff . variations . folded . filtered (has (var_label . only d))) <$> retrieve mid
   let u = var ^. var_url
-      tmpfile = ".cache/" </> unpack mid <> "-" <> d <> "." <> (var ^. var_type)
       key = fromString ("derivatives/" <> unpack mid <> "/" <> d <> "." <> (var ^. var_type))
-  req <- parseRequest u
-  logDbg $ "Downloading " <> mid
-  liftIO $ runConduitRes (httpSource req getResponseBody .| sinkFile tmpfile)
-  awsenv <- newEnv Discover <&> set envRegion NorthVirginia
-  logDbg $ "Uploading " <> mid
-  runResourceT . runAWST awsenv $ do
-    hf <- hashedFile tmpfile
-    void . send $ putObject bucketName key (toBody hf) & poStorageClass ?~ StandardIA
-  liftIO $ removeFile tmpfile
+  logInfo $ "Queueing copy of " <>  mid
+  awsenv <- newEnv Discover <&> set envRegion Oregon
+  runResourceT . runAWST awsenv $
+    void . send $ sendMessage qrl (encodeCopyRequest (pack u) bucketName key)
+
+      where
+        encodeCopyRequest src bname key = TE.decodeUtf8 (BL.toStrict . J.encode $ jbod)
+          where
+            dest = J.Object $ (mempty & at "bucket" ?~ J.String bname
+                               & at "key" ?~ J.String key)
+            jbod = J.Object $ (mempty & at "src" ?~ J.String src
+                               & at "dest" ?~ dest)
 
 runBackup :: GoPro ()
 runBackup = do
   args <- asks (optArgv . gpOptions)
-  when (length args /= 1) $ fail "A bucket name must be specified"
-  let [bucketName] = fromString <$> args
-  have <- Set.fromList . fmap fst <$>  allDerivatives bucketName
+  when (length args /= 2) $ fail "A SQS URL and bucket name must be specified"
+  let [qrl, bucketName] = args
+  have <- Set.fromList . fmap fst <$>  allDerivatives (fromString bucketName)
   logDbg $ "have: " <> (pack . show) have
   want <- Set.fromList <$> loadMediaIDs
-  let todo = Set.toList (want `Set.difference` have)
+  let todo = take 25 $  Set.toList (want `Set.difference` have)
   logDbg $ "todo: " <> (pack.show) todo
   c <- asks (optUploadConcurrency . gpOptions)
-  void $ mapConcurrentlyLimited c (\mid -> storeDerivative bucketName mid "source") todo
+  void $ mapConcurrentlyLimited c (\mid -> storeDerivative (pack qrl) (fromString bucketName) mid "source") todo
