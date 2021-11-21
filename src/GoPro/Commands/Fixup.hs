80bf8e0deea0a4ec6f70042a1f25d5710c244e1d
Dustin Sallings <dustin@spy.net>
2021-11-20 22:29:44 -0800

    `_Object . at` is basically `key`

--- src/GoPro/Commands/Fixup.hs
+++ src/GoPro/Commands/Fixup.hs
@@ -42,8 +42,8 @@ runFixup query = do
               logInfoL ["Fixing ", tshow mid]
               updateMedium (\j -> foldr up j (filter (\(k,_) -> k /= "media_id") stuff)) mid
             up :: (T.Text, SQLData) -> J.Value -> J.Value
-            up (name, SQLInteger i) = _Object . at name ?~ J.Number (fromIntegral i)
-            up (name, SQLFloat i)   = _Object . at name ?~ J.Number (fromFloatDigits i)
-            up (name, SQLText i)    = _Object . at name ?~ J.String i
-            up (name, SQLNull)      = _Object . at name ?~ J.Null
+            up (name, SQLInteger i) = key name .~ J.Number (fromIntegral i)
+            up (name, SQLFloat i)   = key name .~ J.Number (fromFloatDigits i)
+            up (name, SQLText i)    = key name .~ J.String i
+            up (name, SQLNull)      = key name .~ J.Null
             up (_,    SQLBlob _)    = error "can't do blobs"
