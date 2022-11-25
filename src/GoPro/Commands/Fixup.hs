5fa7e98de91b6b0274b8b28a8a8430e6c93ed78b
Dustin Sallings <dustin@spy.net>
2022-11-24 19:09:07 -1000

    Building with lts-20.2

--- src/GoPro/Commands/Fixup.hs
+++ src/GoPro/Commands/Fixup.hs
@@ -4,6 +4,7 @@ import           Control.Lens
 import           Control.Monad.Fail   (MonadFail (..))
 import           Control.Monad.Reader (asks)
 import qualified Data.Aeson           as J
+import qualified Data.Aeson.Key           as J
 import           Data.Aeson.Lens
 import           Data.Foldable        (traverse_)
 import           Data.List            (partition)
@@ -24,4 +25,4 @@ runFixup query = asks database >>= \db -> traverse_ store =<< fixupQuery db quer
                     _ -> fail "no media_id found in result set"
 
     up :: (Text, J.Value) -> J.Value -> J.Value
-    up (k,v) = key k .~ v
+    up (k,v) = key (J.fromText k) .~ v
