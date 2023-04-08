982b9ad74b8bd2b9e243d7da8445f67dbb1942f8
Dustin Sallings <dustin@spy.net>
2023-04-07 21:25:48 -1000

    Flake the ui as well

--- ui/shell.nix
+++ ui/shell.nix
@@ -1,12 +1,12 @@
-with import <nixpkgs> {};
-
-stdenv.mkDerivation {
-  name = "node";
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+mkShell {
   buildInputs = [
     nodePackages.uglify-js
     elmPackages.elm
     gnumake
   ];
+
   shellHook = ''
     export PATH="$PWD/node_modules/.bin/:$PATH"
   '';
